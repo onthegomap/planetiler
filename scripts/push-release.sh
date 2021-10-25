c1c223429a85e900d86e665160e2fcde9e188ef4
Mike Barry <msb5014@gmail.com>
2021-10-25 13:59:39 -0400

    fix release config

--- scripts/push-release.sh
+++ scripts/push-release.sh
@@ -3,8 +3,6 @@ set -euo pipefail
 
 version="${1:-$(mvn help:evaluate -Dexpression=project.version -q -DforceStdout)}"
 
-echo -n "${OSSRH_GPG_SECRET_KEY}" | base64 --decode | gpg --batch --import
-
 docker image push ghcr.io/onthegomap/flatmap:"${version}"
 
 TAGS="${IMAGE_TAGS:-}"
