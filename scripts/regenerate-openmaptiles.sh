8cded564d743e606319e7e2c8fc0ee0168283c90
Mike Barry <msb5014@gmail.com>
2021-12-19 15:54:17 -0500

    run generate test from CI

--- scripts/regenerate-openmaptiles.sh
+++ scripts/regenerate-openmaptiles.sh
@@ -4,12 +4,11 @@ set -o errexit
 set -o pipefail
 set -o nounset
 
-JAR="flatmap-dist/target/*-with-deps.jar"
 TAG="${1:-"v3.12.2"}"
 echo "tag=${TAG}"
 
 echo "Building..."
-./mvnw -DskipTests=true --projects flatmap-basemap -am package
+./mvnw -DskipTests=true --projects flatmap-dist -am package
 
 echo "Running..."
-java -cp "$JAR" com.onthegomap.flatmap.basemap.Generate -tag="${TAG}"
+java -cp flatmap-dist/target/*-with-deps.jar com.onthegomap.flatmap.basemap.Generate -tag="${TAG}"
