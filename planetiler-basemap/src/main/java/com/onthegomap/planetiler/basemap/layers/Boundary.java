89679d0e56ae669b8ef0feb2a2b9345845570176
Mike Barry <msb5014@gmail.com>
2022-01-07 16:46:38 -0500

    boundary layer: show more natural earth boundaries at z4

--- planetiler-basemap/src/main/java/com/onthegomap/planetiler/basemap/layers/Boundary.java
+++ planetiler-basemap/src/main/java/com/onthegomap/planetiler/basemap/layers/Boundary.java
@@ -168,7 +168,9 @@ public class Boundary implements
         : new BoundaryInfo(2, 4, 4);
       case "ne_10m_admin_1_states_provinces_lines" -> {
         Double minZoom = Parse.parseDoubleOrNull(feature.getTag("min_zoom"));
-        yield minZoom != null && minZoom <= 7 ? new BoundaryInfo(4, 1, 4) : null;
+        yield minZoom != null && minZoom <= 7 ? new BoundaryInfo(4, 1, 4) :
+          minZoom != null && minZoom <= 7.7 ? new BoundaryInfo(4, 4, 4) :
+            null;
       }
       default -> null;
     };
