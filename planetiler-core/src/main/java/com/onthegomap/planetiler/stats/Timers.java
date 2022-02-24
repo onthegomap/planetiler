e95480adf55b6d5f728a7f2414d7160c6512a7f0
Mike Barry <msb5014@gmail.com>
2022-02-24 06:52:59 -0500

    log thread details

--- planetiler-core/src/main/java/com/onthegomap/planetiler/stats/Timers.java
+++ planetiler-core/src/main/java/com/onthegomap/planetiler/stats/Timers.java
@@ -2,6 +2,7 @@ package com.onthegomap.planetiler.stats;
 
 import com.onthegomap.planetiler.util.Format;
 import java.time.Duration;
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.LinkedHashMap;
 import java.util.List;
@@ -37,28 +38,49 @@ public class Timers {
       String name = entry.getKey();
       var elapsed = entry.getValue().timer.elapsed();
       LOGGER.info("\t" + name + "\t" + elapsed);
-      printStageDetails(name, "\t  ");
+      for (String detail : getStageDetails(name)) {
+        LOGGER.info("\t  " + detail);
+      }
     }
   }
 
-  private void printStageDetails(String name, String prefix) {
+  private List<String> getStageDetails(String name) {
+    List<String> resultList = new ArrayList<>();
     Stage stage = timers.get(name);
     var elapsed = stage.timer.elapsed();
     List<String> threads = stage.threadStats.stream().map(d -> d.prefix).distinct().toList();
     for (String thread : threads) {
+      StringBuilder result = new StringBuilder();
       List<ProcessInfo.ThreadState> threadStates = stage.threadStats.stream().filter(t -> t.prefix.equals(thread))
         .map(t -> t.state).toList();
+      int num = threadStates.size();
       ProcessInfo.ThreadState sum = threadStates.stream().reduce(ProcessInfo.ThreadState.DEFAULT,
         ProcessInfo.ThreadState::sum);
-      double totalNanos = elapsed.wall().multipliedBy(threadStates.size()).toNanos();
+      double totalNanos = elapsed.wall().multipliedBy(num).toNanos();
+      result.append(thread.replace(name + "_", ""))
+        .append("(")
+        .append(num)
+        .append("x")
+        .append(FORMAT.percent(sum.cpuTime().toNanos() / totalNanos))
+        .append(" cpu:")
+        .append(FORMAT.duration(sum.cpuTime().dividedBy(num)));
 
-      LOGGER.debug(prefix + thread.replace(name + "_", "") + " x" + threadStates.size() + "\t" +
-        FORMAT.duration(sum.cpuTime()) +
-        " (" + FORMAT.percent(sum.cpuTime().toNanos() / totalNanos) + ")" +
-        (sum.blocking().compareTo(Duration.ofSeconds(1)) > 0 ? " blocked:" + FORMAT.duration(sum.blocking()) : "") +
-        (sum.waiting().compareTo(Duration.ofSeconds(1)) > 0 ? " waiting:" + FORMAT.duration(sum.waiting()) : "")
-      );
+      Duration systemTime = sum.cpuTime().minus(sum.userTime()).dividedBy(num);
+      if (systemTime.compareTo(Duration.ofSeconds(1)) > 0) {
+        result.append(" sys:").append(FORMAT.duration(systemTime));
+      }
+      Duration blockTime = sum.blocking().dividedBy(num);
+      if (blockTime.compareTo(Duration.ofSeconds(1)) > 0) {
+        result.append(" block:").append(FORMAT.duration(blockTime));
+      }
+      Duration waitTime = sum.waiting().dividedBy(num);
+      if (waitTime.compareTo(Duration.ofSeconds(1)) > 0) {
+        result.append(" wait:").append(FORMAT.duration(waitTime));
+      }
+      result.append(")");
+      resultList.add(result.toString());
     }
+    return resultList;
   }
 
   public Finishable startTimer(String name) {
@@ -70,7 +92,7 @@ public class Timers {
     LOGGER.info("Starting...");
     return () -> {
       LOGGER.info("Finished in " + timers.get(name).timer.stop());
-      printStageDetails(name, "  ");
+      LOGGER.info("  " + String.join(" -> ", getStageDetails(name)));
       currentStage.set(last);
     };
   }
