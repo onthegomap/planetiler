6e9210e013858a3f36f8e011a91c5ea1d9102140
Mike Barry <msb5014@gmail.com>
2022-02-28 21:29:10 -0500

    upgrade hppc

--- planetiler-core/src/main/java/com/onthegomap/planetiler/collection/Hppc.java
+++ planetiler-core/src/main/java/com/onthegomap/planetiler/collection/Hppc.java
@@ -1,8 +1,6 @@
 package com.onthegomap.planetiler.collection;
 
 
-import com.carrotsearch.hppc.HashOrderMixing;
-import com.carrotsearch.hppc.HashOrderMixingStrategy;
 import com.carrotsearch.hppc.IntObjectHashMap;
 import com.carrotsearch.hppc.LongByteHashMap;
 import com.carrotsearch.hppc.LongByteMap;
@@ -13,33 +11,31 @@ import com.carrotsearch.hppc.ObjectIntHashMap;
 
 public class Hppc {
 
-  private static final HashOrderMixingStrategy DETERMINISTIC = HashOrderMixing.constant(123321123321123312L);
-
   public static <T> IntObjectHashMap<T> newIntObjectHashMap() {
-    return new IntObjectHashMap<>(10, 0.75, DETERMINISTIC);
+    return new IntObjectHashMap<>(10, 0.75);
   }
 
   public static <T> ObjectIntHashMap<T> newObjectIntHashMap() {
-    return new ObjectIntHashMap<>(10, 0.75, DETERMINISTIC);
+    return new ObjectIntHashMap<>(10, 0.75);
   }
 
   public static LongLongHashMap newLongLongHashMap() {
-    return new LongLongHashMap(10, 0.75, DETERMINISTIC);
+    return new LongLongHashMap(10, 0.75);
   }
 
   public static <T> LongObjectHashMap<T> newLongObjectHashMap() {
-    return new LongObjectHashMap<>(10, 0.75, DETERMINISTIC);
+    return new LongObjectHashMap<>(10, 0.75);
   }
 
   public static <T> LongObjectHashMap<T> newLongObjectHashMap(int size) {
-    return new LongObjectHashMap<>(size, 0.75, DETERMINISTIC);
+    return new LongObjectHashMap<>(size, 0.75);
   }
 
   public static LongIntHashMap newLongIntHashMap() {
-    return new LongIntHashMap(10, 0.75, DETERMINISTIC);
+    return new LongIntHashMap(10, 0.75);
   }
 
   public static LongByteMap newLongByteHashMap() {
-    return new LongByteHashMap(10, 0.75, DETERMINISTIC);
+    return new LongByteHashMap(10, 0.75);
   }
 }
