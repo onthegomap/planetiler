10ecf2502cb681e0e85d2fb6d548e3fcf70ad00c
Mike Barry <msb5014@gmail.com>
2022-03-01 07:21:25 -0500

    fail fast

--- planetiler-core/src/test/java/com/onthegomap/planetiler/util/NativeUtilTest.java
+++ planetiler-core/src/test/java/com/onthegomap/planetiler/util/NativeUtilTest.java
@@ -1,14 +1,7 @@
 package com.onthegomap.planetiler.util;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
 import java.io.IOException;
-import java.nio.MappedByteBuffer;
-import java.nio.channels.FileChannel;
-import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
 import java.nio.file.Path;
-import java.nio.file.StandardOpenOption;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.io.TempDir;
 
@@ -16,16 +9,17 @@ public class NativeUtilTest {
 
   @Test
   public void testMadvise(@TempDir Path dir) throws IOException {
-    String data = "test";
-    int bytes = data.getBytes(StandardCharsets.UTF_8).length;
-    var path = dir.resolve("file");
-    Files.writeString(path, data, StandardOpenOption.CREATE, StandardOpenOption.WRITE);
-    try (FileChannel channel = FileChannel.open(path, StandardOpenOption.READ)) {
-      MappedByteBuffer buffer = channel.map(FileChannel.MapMode.READ_ONLY, 0, bytes);
-      NativeUtil.madvise(buffer, NativeUtil.Madvice.RANDOM);
-      byte[] received = new byte[bytes];
-      buffer.get(received);
-      assertEquals(data, new String(received, StandardCharsets.UTF_8));
-    }
+    throw new Error(System.getProperty("os.name"));
+//    String data = "test";
+//    int bytes = data.getBytes(StandardCharsets.UTF_8).length;
+//    var path = dir.resolve("file");
+//    Files.writeString(path, data, StandardOpenOption.CREATE, StandardOpenOption.WRITE);
+//    try (FileChannel channel = FileChannel.open(path, StandardOpenOption.READ)) {
+//      MappedByteBuffer buffer = channel.map(FileChannel.MapMode.READ_ONLY, 0, bytes);
+//      NativeUtil.madvise(buffer, NativeUtil.Madvice.RANDOM);
+//      byte[] received = new byte[bytes];
+//      buffer.get(received);
+//      assertEquals(data, new String(received, StandardCharsets.UTF_8));
+//    }
   }
 }
