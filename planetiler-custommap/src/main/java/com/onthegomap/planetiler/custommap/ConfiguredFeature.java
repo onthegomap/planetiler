3dde7c8a3405bae8287987fcf1f5bf699b149f91
Brian Sperlongano <zelonewolf@gmail.com>
2022-04-09 13:51:02 -0400

    Add documentation

--- planetiler-custommap/src/main/java/com/onthegomap/planetiler/custommap/ConfiguredFeature.java
+++ planetiler-custommap/src/main/java/com/onthegomap/planetiler/custommap/ConfiguredFeature.java
@@ -25,6 +25,9 @@ import java.util.function.BiConsumer;
 import java.util.function.Function;
 import java.util.function.Predicate;
 
+/**
+ * A map feature, configured from a YML configuration file
+ */
 public class ConfiguredFeature {
 
   private Collection<String> sources;
@@ -44,17 +47,32 @@ public class ConfiguredFeature {
 
     FeatureGeometryType geometryType = includeWhen.getGeometry();
 
+    //Test to determine whether this type of geometry is included
     geometryTest = geometryTest(geometryType);
-    zoomConfig = zoomFilter(feature.getZoom());
-    geometryFactory = geometryMapFeature(layerName, geometryType);
 
+    //Test to determine whether this feature is included based on tagging
     tagTest = tagTest(includeWhen.getTag());
 
+    //Test to determine at which zooms to include this feature based on tagging
+    zoomConfig = zoomFilter(feature.getZoom());
+
+    //Factory to generate the right feature type from FeatureCollector
+    geometryFactory = geometryMapFeature(layerName, geometryType);
+
+    //Configure logic for each attribute in the output tile
     feature.getAttributes().forEach(attribute -> {
       attributeProcessors.add(attributeProcessor(attribute));
     });
   }
 
+  /**
+   * Generate zoom-based inclusion logic based on tagging and overall zoom limits. Tag-based zoom logic will override
+   * universal zoom logic. The returned lambda will configure zoom limits for a specified {@link SourceFeature} by
+   * invoking the appropriate methods in {@link Feature}.
+   * 
+   * @param zoomConfig zoom configuration
+   * @return processing logic
+   */
   private BiConsumer<SourceFeature, Feature> zoomFilter(ZoomConfig zoomConfig) {
 
     Collection<ZoomFilter> zfList = zoomConfig.getZoomFilter();
@@ -77,6 +95,15 @@ public class ConfiguredFeature {
     };
   }
 
+  /**
+   * Produce logic that generates attribute values based on configuration and input data. If both a constantValue
+   * configuration and a tagValue configuration are set, this is likely a mistake, and the constantValue will take
+   * precedence.
+   * 
+   * @param attribute - attribute definition configured from YML
+   * @return a function that generates an attribute value from a {@link SourceFeature} based on an attribute
+   *         configuration.
+   */
   static Function<SourceFeature, Object> attributeValueProducer(AttributeDefinition attribute) {
 
     String constVal = attribute.getConstantValue();
@@ -92,6 +119,13 @@ public class ConfiguredFeature {
     throw new IllegalArgumentException("No value producer specified");
   }
 
+  /**
+   * Performs data type normalization in cases where there are value variations. For example, yes/1/true all mean the
+   * same thing if a tag is boolean in nature.
+   * 
+   * @param attributeDataType data type to normalize
+   * @return lambda that converts raw tag values to normalized attribute values
+   */
   private static Function<Object, Object> typeConverter(AttributeDataType attributeDataType) {
 
     if (Objects.isNull(attributeDataType)) {
@@ -112,6 +146,11 @@ public class ConfiguredFeature {
   private static Set<String> booleanTrue = new HashSet<>(Arrays.<String>asList("true", "yes", "1"));
   private static Set<String> booleanFalse = new HashSet<>(Arrays.<String>asList("false", "no", "0"));
 
+  /**
+   * Convert a raw tag value to a corresponding normalized boolean value.
+   * 
+   * @return normalization function
+   */
   private static Function<Object, Object> booleanTypeConverter() {
     return s -> {
       if (booleanTrue.contains(s)) {
@@ -124,6 +163,13 @@ public class ConfiguredFeature {
     };
   }
 
+  /**
+   * Generate logic which determines the minimum zoom level for a feature based on a configured pixel size limit.
+   * 
+   * @param minTilePercent - minimum percentage of a tile that a feature must cover to be shown
+   * @param minZoom        - global minimum zoom for this feature
+   * @return minimum zoom function
+   */
   private static Function<SourceFeature, Integer> attributeZoomThreshold(double minTilePercent, int minZoom) {
     return sf -> {
       try {
@@ -135,6 +181,12 @@ public class ConfiguredFeature {
     };
   };
 
+  /**
+   * Generates a function which produces a fully-configured attribute for a feature.
+   * 
+   * @param attribute - configuration for this attribute
+   * @return processing logic
+   */
   private BiConsumer<SourceFeature, Feature> attributeProcessor(AttributeDefinition attribute) {
     String tagKey = attribute.getKey();
     Integer configuredMinZoom = attribute.getMinZoom();
@@ -167,6 +219,14 @@ public class ConfiguredFeature {
     };
   }
 
+  /**
+   * Generates a factory method which creates a {@link Feature} from a {@link FeatureCollector} of the appropriate
+   * geometry type.
+   * 
+   * @param layerName - name of the layer
+   * @param type      - type of geometry
+   * @return geometry factory method
+   */
   private static Function<FeatureCollector, Feature> geometryMapFeature(String layerName, FeatureGeometryType type) {
     switch (type) {
       case polygon:
@@ -180,6 +240,12 @@ public class ConfiguredFeature {
     }
   }
 
+  /**
+   * Generate a test for whether a source feature is of the correct geometry to be included in the tile.
+   * 
+   * @param type type of geometry
+   * @return geometry test method
+   */
   private static Predicate<SourceFeature> geometryTest(FeatureGeometryType type) {
     switch (type) {
       case polygon:
@@ -193,6 +259,13 @@ public class ConfiguredFeature {
     }
   }
 
+  /**
+   * Generate a test for whether a feature should be included on the basis of its tagging.
+   * 
+   * @param attrIncludeWhen        - criteria for whether to include a tag
+   * @param attributeValueProducer - method for generating the attribute value from raw tagging
+   * @return tagging test method
+   */
   static Predicate<SourceFeature> attributeTagTest(FeatureCriteria attrIncludeWhen,
     Function<SourceFeature, Object> attributeValueProducer) {
 
@@ -216,6 +289,12 @@ public class ConfiguredFeature {
     return test;
   }
 
+  /**
+   * Generate a test for whether a {@link SourceFeature} matches a specified tag criteria.
+   * 
+   * @param tagCriteria - tag criteria
+   * @return tag test method
+   */
   private static Predicate<SourceFeature> tagTest(TagCriteria tagCriteria) {
     if (tagCriteria == null) {
       return sf -> true;
@@ -225,18 +304,20 @@ public class ConfiguredFeature {
     if (keyTest == null) {
       return sf -> true;
     }
-    return tagTest(keyTest, valTest);
-  }
-
-  private static Predicate<SourceFeature> tagTest(String key, Collection<String> values) {
     return sf -> {
-      if (sf.hasTag(key)) {
-        return values.contains(sf.getTag(key).toString());
+      if (sf.hasTag(keyTest)) {
+        return valTest.contains(sf.getTag(keyTest).toString());
       }
       return false;
     };
   }
 
+  /**
+   * Test to determine whether to include a source feature in the tiles
+   * 
+   * @param sourceFeature - source feature
+   * @return true if the feature will be rendered
+   */
   public boolean includeWhen(SourceFeature sourceFeature) {
     //Is this from the right source?
     if (!sources.contains(sourceFeature.getSource())) {
@@ -252,6 +333,12 @@ public class ConfiguredFeature {
     return tagTest.test(sourceFeature);
   }
 
+  /**
+   * Generate a tile feature based on a source feature
+   * 
+   * @param sourceFeature - input source feature
+   * @param features      - output rendered feature collector
+   */
   public void processFeature(SourceFeature sourceFeature, FeatureCollector features) {
 
     Feature f = geometryFactory.apply(features);
