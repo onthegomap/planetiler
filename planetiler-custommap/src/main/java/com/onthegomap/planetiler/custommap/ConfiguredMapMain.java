58da97570ad0e655717e01fa347a839775875d58
Brian Sperlongano <zelonewolf@gmail.com>
2022-04-10 21:51:47 -0400

    Default output to data/ folder

--- planetiler-custommap/src/main/java/com/onthegomap/planetiler/custommap/ConfiguredMapMain.java
+++ planetiler-custommap/src/main/java/com/onthegomap/planetiler/custommap/ConfiguredMapMain.java
@@ -1,17 +1,19 @@
 package com.onthegomap.planetiler.custommap;
 
-import com.onthegomap.planetiler.Planetiler;
-import com.onthegomap.planetiler.config.Arguments;
-import com.onthegomap.planetiler.custommap.configschema.DataSource;
-import com.onthegomap.planetiler.custommap.configschema.DataSourceType;
-import com.onthegomap.planetiler.custommap.configschema.SchemaConfig;
 import java.io.File;
 import java.io.FileInputStream;
 import java.net.URI;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Collection;
+
 import org.yaml.snakeyaml.Yaml;
+
+import com.onthegomap.planetiler.Planetiler;
+import com.onthegomap.planetiler.config.Arguments;
+import com.onthegomap.planetiler.custommap.configschema.DataSource;
+import com.onthegomap.planetiler.custommap.configschema.DataSourceType;
+import com.onthegomap.planetiler.custommap.configschema.SchemaConfig;
 
 public class ConfiguredMapMain {
 
@@ -36,12 +38,6 @@ public class ConfiguredMapMain {
       return;
     }
 
-    String mbtilesOutput = args.getString(
-      "mbtiles",
-      "Location to store output mbtiles",
-      "output.mbtiles"
-    );
-
     Yaml yml = new Yaml();
     SchemaConfig config = yml.loadAs(new FileInputStream(new File(schemaFile)), SchemaConfig.class);
 
@@ -53,7 +49,7 @@ public class ConfiguredMapMain {
       configureSource(planetiler, sourcesDir, source);
     }
 
-    planetiler.overwriteOutput("mbtiles", Path.of(mbtilesOutput))
+    planetiler.overwriteOutput("mbtiles", Path.of("data", "output.mbtiles"))
       .run();
   }
 
