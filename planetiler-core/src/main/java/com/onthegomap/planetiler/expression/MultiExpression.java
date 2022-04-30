83fed7965ff4be0822887c5b702816c49a7eb34b
Brian Sperlongano <zelonewolf@gmail.com>
2022-04-29 21:54:23 -0400

    Bugfix

--- planetiler-core/src/main/java/com/onthegomap/planetiler/expression/MultiExpression.java
+++ planetiler-core/src/main/java/com/onthegomap/planetiler/expression/MultiExpression.java
@@ -68,8 +68,8 @@ public record MultiExpression<T> (List<Entry<T>> expressions) {
       and.children().forEach(child -> getRelevantKeys(child, acceptKey));
     } else if (exp instanceof Expression.Or or) {
       or.children().forEach(child -> getRelevantKeys(child, acceptKey));
-    } else if (exp instanceof Expression.Not) {
-      // ignore anything that's purely used as a filter
+    } else if (exp instanceof Expression.Not not) {
+      getRelevantMissingKeys(not.child(), acceptKey);
     } else if (exp instanceof Expression.MatchField field) {
       acceptKey.accept(field.field());
     } else if (exp instanceof Expression.MatchAny any) {
@@ -86,8 +86,8 @@ public record MultiExpression<T> (List<Entry<T>> expressions) {
       and.children().forEach(child -> getRelevantMissingKeys(child, acceptKey));
     } else if (exp instanceof Expression.Or or) {
       or.children().forEach(child -> getRelevantMissingKeys(child, acceptKey));
-    } else if (exp instanceof Expression.Not) {
-      // ignore anything that's purely used as a filter
+    } else if (exp instanceof Expression.Not not) {
+      getRelevantKeys(not.child(), acceptKey);
     } else if (exp instanceof Expression.MatchAny any && any.matchWhenMissing()) {
       acceptKey.accept(any.field());
     }
