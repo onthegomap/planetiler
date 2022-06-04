9c444e5b42a9de722c0916dd604b96d43c9cd18f
Mike Barry <msb5014@gmail.com>
2022-06-03 20:22:40 -0400

    add test

--- planetiler-core/src/test/java/com/onthegomap/planetiler/FeatureMergeTest.java
+++ planetiler-core/src/test/java/com/onthegomap/planetiler/FeatureMergeTest.java
@@ -122,6 +122,23 @@ class FeatureMergeTest {
   }
 
   @Test
+  void simplifyLineStringIfToleranceIsSet() {
+    assertEquals(
+      List.of(
+        feature(1, newLineString(10, 10, 30, 30), Map.of("a", 1))
+      ),
+      FeatureMerge.mergeLineStrings(
+        List.of(
+          feature(1, newLineString(10, 10, 20, 20, 30, 30), Map.of("a", 1))
+        ),
+        0,
+        1,
+        0
+      )
+    );
+  }
+
+  @Test
   void mergeMultiLineString() {
     assertEquals(
       List.of(
