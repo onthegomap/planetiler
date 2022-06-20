a373ec028086cf8baeb0cabf4f16048692ed4e0d
Martin Guillon <martin.guillon@akylas.fr>
2022-06-20 20:31:18 +0200

    chore: indent fix

--- planetiler-core/src/main/java/com/onthegomap/planetiler/reader/osm/PolyFileReader.java
+++ planetiler-core/src/main/java/com/onthegomap/planetiler/reader/osm/PolyFileReader.java
@@ -16,67 +16,67 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class PolyFileReader {
-    private static final Logger LOGGER = LoggerFactory.getLogger(PolyFileReader.class);
+  private static final Logger LOGGER = LoggerFactory.getLogger(PolyFileReader.class);
 
-    private PolyFileReader() {
-        throw new IllegalStateException("Utility class");
+  private PolyFileReader() {
+    throw new IllegalStateException("Utility class");
+  }
+
+  public static MultiPolygon parsePolyFile(String filePath) throws NumberFormatException, IOException {
+    if (filePath == null) {
+      return null;
     }
 
-    public static MultiPolygon parsePolyFile(String filePath) throws NumberFormatException, IOException {
-        if (filePath == null) {
-            return null;
-        }
-
-        File file = new File(filePath);
-        try (
-            BufferedReader br =
-            new BufferedReader(new FileReader(file))
-        ) {
-            boolean inRing = false;
-            String line;
-            List<Polygon> polygons = new ArrayList<>();
-            List<Coordinate> currentRing = null;
-            boolean firstLine = true;
-            while ((line = br.readLine()) != null) {
-                if (firstLine) {
-                    firstLine = false;
-                    // first line is junk.
-                } else if (inRing) {
-                    if (line.strip().equals("END")) {
-                        // we are at the end of a ring, perhaps with more to come.
-                        if (currentRing != null) {
-                            polygons.add(JTS_FACTORY
-                                .createPolygon(
-                                    JTS_FACTORY
-                                        .createLinearRing(currentRing.toArray(new Coordinate[currentRing.size()])),
-                                    null));
-                            currentRing = null;
-                        }
-                        inRing = false;
-                    } else if (currentRing != null) {
-                        // we are in a ring and picking up new coordinates.
-                        String[] splitted = line.trim().split("\s+");
-                        currentRing.add(new CoordinateXY(Float.parseFloat(splitted[0]), Float.parseFloat(splitted[1])));
-                    }
-                } else {
-                    if (line.strip().equals("END")) {
-                        // we are at the end of the whole polygon.
-                        break;
-                    } else if (line.charAt(0) == '!') {
-                        // we ignore holes for now
-                        inRing = true;
-                    } else {
-                        // we are at the start of a polygon part.
-                        currentRing = new ArrayList<>();
-                        inRing = true;
-                    }
-                }
+    File file = new File(filePath);
+    try (
+      BufferedReader br =
+        new BufferedReader(new FileReader(file))
+    ) {
+      boolean inRing = false;
+      String line;
+      List<Polygon> polygons = new ArrayList<>();
+      List<Coordinate> currentRing = null;
+      boolean firstLine = true;
+      while ((line = br.readLine()) != null) {
+        if (firstLine) {
+          firstLine = false;
+          // first line is junk.
+        } else if (inRing) {
+          if (line.strip().equals("END")) {
+            // we are at the end of a ring, perhaps with more to come.
+            if (currentRing != null) {
+              polygons.add(JTS_FACTORY
+                .createPolygon(
+                  JTS_FACTORY
+                    .createLinearRing(currentRing.toArray(new Coordinate[currentRing.size()])),
+                  null));
+              currentRing = null;
             }
-
-            return JTS_FACTORY.createMultiPolygon(polygons.toArray(new Polygon[polygons.size()]));
-        } catch (Exception e) {
-            LOGGER.error("Failed to parse poly file {} : {}", filePath, e);
-            return null;
+            inRing = false;
+          } else if (currentRing != null) {
+            // we are in a ring and picking up new coordinates.
+            String[] splitted = line.trim().split("\s+");
+            currentRing.add(new CoordinateXY(Float.parseFloat(splitted[0]), Float.parseFloat(splitted[1])));
+          }
+        } else {
+          if (line.strip().equals("END")) {
+            // we are at the end of the whole polygon.
+            break;
+          } else if (line.charAt(0) == '!') {
+            // we ignore holes for now
+            inRing = true;
+          } else {
+            // we are at the start of a polygon part.
+            currentRing = new ArrayList<>();
+            inRing = true;
+          }
         }
+      }
+
+      return JTS_FACTORY.createMultiPolygon(polygons.toArray(new Polygon[polygons.size()]));
+    } catch (Exception e) {
+      LOGGER.error("Failed to parse poly file {} : {}", filePath, e);
+      return null;
     }
+  }
 }
