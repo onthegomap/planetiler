9110378062a1b26438a43a899f377fd4ad4494aa
Brandon Liu <bdon@bdon.org>
2022-06-21 21:27:32 +0800

    add faster hilbert implementation and benchmark [#128]

--- planetiler-core/src/main/java/com/onthegomap/planetiler/geo/TileCoord.java
+++ planetiler-core/src/main/java/com/onthegomap/planetiler/geo/TileCoord.java
@@ -22,8 +22,6 @@ import org.locationtech.jts.geom.CoordinateXY;
 public record TileCoord(int encoded, int x, int y, int z) implements Comparable<TileCoord> {
   // z16+ would need more space
 
-  private static final int XY_MASK = (1 << 14) - 1;
-
   public TileCoord {
     assert z <= 15;
   }
@@ -34,43 +32,129 @@ public record TileCoord(int encoded, int x, int y, int z) implements Comparable<
 
   public static TileCoord decode(int encoded) {
     int acc = 0;
-    int tmp_z = 0;
+    int tmpZ = 0;
     while (true) {
-      int num_tiles = (1 << tmp_z) * (1 << tmp_z);
-      if (acc + num_tiles > encoded) {
+      int numTiles = (1 << tmpZ) * (1 << tmpZ);
+      if (acc + numTiles > encoded) {
         int position = encoded - acc;
-        return decodeOnLevel(tmp_z, position, encoded);
+        long xy = hilbertIndexToXY(tmpZ, position);
+        return new TileCoord(encoded, (int) (xy >>> 32 & 0xFFFFFFFFL), (int) (xy & 0xFFFFFFFFL), tmpZ);
       }
-      acc += num_tiles;
-      tmp_z++;
+      acc += numTiles;
+      tmpZ++;
     }
   }
 
-  private static void rotate(int n, int[] xy, int rx, int ry) {
-    if (ry == 0) {
-      if (rx == 1) {
-        xy[0] = n - 1 - xy[0];
-        xy[1] = n - 1 - xy[1];
-      }
-      int t = xy[0];
-      xy[0] = xy[1];
-      xy[1] = t;
-    }
+  // Fast Hilbert curve algorithm by http://threadlocalmutex.com/
+  // Ported from C++ https://github.com/rawrunprotected/hilbert_curves (public domain)
+  private static int deinterleave(int tx) {
+    tx = tx & 0x55555555;
+    tx = (tx | (tx >>> 1)) & 0x33333333;
+    tx = (tx | (tx >>> 2)) & 0x0F0F0F0F;
+    tx = (tx | (tx >>> 4)) & 0x00FF00FF;
+    tx = (tx | (tx >>> 8)) & 0x0000FFFF;
+    return tx;
   }
 
-  private static TileCoord decodeOnLevel(int z, int position, int encoded) {
-    int n = 1 << z;
-    int rx, ry, t = position;
-    int[] xy = {0, 0};
-    for (int s = 1; s < n; s *= 2) {
-      rx = 1 & Integer.divideUnsigned(t, 2);
-      ry = 1 & (t ^ rx);
-      rotate(s, xy, rx, ry);
-      xy[0] += s * rx;
-      xy[1] += s * ry;
-      t = Integer.divideUnsigned(t, 4);
+  private static int interleave(int tx) {
+    tx = (tx | (tx << 8)) & 0x00FF00FF;
+    tx = (tx | (tx << 4)) & 0x0F0F0F0F;
+    tx = (tx | (tx << 2)) & 0x33333333;
+    tx = (tx | (tx << 1)) & 0x55555555;
+    return tx;
+  }
+
+  private static int prefixScan(int tx) {
+    tx = (tx >>> 8) ^ tx;
+    tx = (tx >>> 4) ^ tx;
+    tx = (tx >>> 2) ^ tx;
+    tx = (tx >>> 1) ^ tx;
+    return tx;
+  }
+
+  private static long hilbertIndexToXY(int n, int i) {
+    i = i << (32 - 2 * n);
+
+    int i0 = deinterleave(i);
+    int i1 = deinterleave(i >>> 1);
+
+    int t0 = (i0 | i1) ^ 0xFFFF;
+    int t1 = i0 & i1;
+
+    int prefixT0 = prefixScan(t0);
+    int prefixT1 = prefixScan(t1);
+
+    int a = (((i0 ^ 0xFFFF) & prefixT1) | (i0 & prefixT0));
+
+    int resultX = (a ^ i1) >>> (16 - n);
+    int resultY = (a ^ i0 ^ i1) >>> (16 - n);
+    return ((long) resultX << 32) | resultY;
+  }
+
+  // Ignore warnings about nested scopes.
+  @SuppressWarnings("java:S1199")
+  private static int hilbertXYToIndex(int n, int tx, int ty) {
+    tx = tx << (16 - n);
+    ty = ty << (16 - n);
+
+    int hA, hB, hC, hD;
+
+    {
+      int a = tx ^ ty;
+      int b = 0xFFFF ^ a;
+      int c = 0xFFFF ^ (tx | ty);
+      int d = tx & (ty ^ 0xFFFF);
+
+      hA = a | (b >>> 1);
+      hB = (a >>> 1) ^ a;
+
+      hC = ((c >>> 1) ^ (b & (d >>> 1))) ^ c;
+      hD = ((a & (c >>> 1)) ^ (d >>> 1)) ^ d;
     }
-    return new TileCoord(encoded, xy[0], xy[1], z);
+
+    {
+      int a = hA;
+      int b = hB;
+      int c = hC;
+      int d = hD;
+
+      hA = ((a & (a >>> 2)) ^ (b & (b >>> 2)));
+      hB = ((a & (b >>> 2)) ^ (b & ((a ^ b) >>> 2)));
+
+      hC ^= ((a & (c >>> 2)) ^ (b & (d >>> 2)));
+      hD ^= ((b & (c >>> 2)) ^ ((a ^ b) & (d >>> 2)));
+    }
+
+    {
+      int a = hA;
+      int b = hB;
+      int c = hC;
+      int d = hD;
+
+      hA = ((a & (a >>> 4)) ^ (b & (b >>> 4)));
+      hB = ((a & (b >>> 4)) ^ (b & ((a ^ b) >>> 4)));
+
+      hC ^= ((a & (c >>> 4)) ^ (b & (d >>> 4)));
+      hD ^= ((b & (c >>> 4)) ^ ((a ^ b) & (d >>> 4)));
+    }
+
+    {
+      int a = hA;
+      int b = hB;
+      int c = hC;
+      int d = hD;
+
+      hC ^= ((a & (c >>> 8)) ^ (b & (d >>> 8)));
+      hD ^= ((b & (c >>> 8)) ^ ((a ^ b) & (d >>> 8)));
+    }
+
+    int a = hC ^ (hC >>> 1);
+    int b = hD ^ (hD >>> 1);
+
+    int i0 = tx ^ ty;
+    int i1 = b | (0xFFFF ^ (i0 | a));
+
+    return ((interleave(i1) << 1) | interleave(i0)) >>> (32 - 2 * n);
   }
 
   /** Returns the tile containing a latitude/longitude coordinate at a given zoom level. */
@@ -83,19 +167,10 @@ public record TileCoord(int encoded, int x, int y, int z) implements Comparable<
 
   public static int encode(int x, int y, int z) {
     int acc = 0;
-    for (int tmp_z = 0; tmp_z < z; tmp_z++) {
-      acc += (1 << tmp_z) * (1 << tmp_z);
+    for (int tmpZ = 0; tmpZ < z; tmpZ++) {
+      acc += (1 << tmpZ) * (1 << tmpZ);
     }
-    int n = 1 << z;
-    int rx, ry, d = 0;
-    int[] xy = {x, y};
-    for (int s = Integer.divideUnsigned(n, 2); s > 0; s = Integer.divideUnsigned(s, 2)) {
-      rx = (xy[0] & s) > 0 ? 1 : 0;
-      ry = (xy[1] & s) > 0 ? 1 : 0;
-      d += s * s * ((3 * rx) ^ ry);
-      rotate(s, xy, rx, ry);
-    }
-    return acc + d;
+    return acc + hilbertXYToIndex(z, x, y);
   }
 
   @Override
