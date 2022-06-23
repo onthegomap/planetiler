31c6304efd371c1e10f9948c159666690acf8fbe
Mike Barry <msb5014@gmail.com>
2022-06-23 09:00:12 -0400

    update language utils

--- planetiler-core/src/main/java/com/onthegomap/planetiler/util/LanguageUtils.java
+++ planetiler-core/src/main/java/com/onthegomap/planetiler/util/LanguageUtils.java
@@ -6,11 +6,12 @@ import java.util.Map;
 import java.util.Set;
 import java.util.function.Predicate;
 import java.util.regex.Pattern;
+import java.util.stream.Stream;
 
 public class LanguageUtils {
   // Name tags that should be eligible for finding a latin name.
   // See https://wiki.openstreetmap.org/wiki/Multilingual_names
-  public static final Predicate<String> VALID_NAME_TAGS =
+  private static final Predicate<String> VALID_NAME_TAGS =
     Pattern
       .compile("^name:[a-z]{2,3}(-[a-z]{4})?([-_](x-)?[a-z]{2,})?(-([a-z]{2}|[0-9]{3}))?$", Pattern.CASE_INSENSITIVE)
       .asMatchPredicate();
@@ -24,14 +25,10 @@ public class LanguageUtils {
   private static final Pattern EMPTY_PARENS = Pattern.compile("(\\([ -.]*\\)|\\[[ -.]*])");
   private static final Pattern LEADING_TRAILING_JUNK = Pattern.compile("((^[\\s./-]*)|([\\s./-]*$))");
   private static final Pattern WHITESPACE = Pattern.compile("\\s+");
-  public static final Set<String> EN_DE_NAME_KEYS = Set.of("name:en", "name:de");
+  private static final Set<String> EN_DE_NAME_KEYS = Set.of("name:en", "name:de");
 
-  public LanguageUtils() {}
-
-  public static void putIfNotEmpty(Map<String, Object> dest, String key, Object value) {
-    if (value != null && !value.equals("")) {
-      dest.put(key, value);
-    }
+  private LanguageUtils() {
+    throw new IllegalStateException("Utility class");
   }
 
   public static String string(Object obj) {
@@ -40,10 +37,6 @@ public class LanguageUtils {
 
   public static boolean containsOnlyLatinCharacters(String string) {
     return string != null && ONLY_LATIN.test(string);
-  }
-
-  public static String transliteratedName(Map<String, Object> tags) {
-    return Translations.transliterate(string(tags.get("name")));
   }
 
   public static String removeLatinCharacters(String name) {
@@ -58,8 +51,36 @@ public class LanguageUtils {
       // then remove any of those extra characters now
       result = EMPTY_PARENS.matcher(result).replaceAll("");
       result = LEADING_TRAILING_JUNK.matcher(result).replaceAll("");
-      return WHITESPACE.matcher(result).replaceAll(" ");
+      result = WHITESPACE.matcher(result).replaceAll(" ").trim();
+      return result.isBlank() ? null : result;
     }
     return name.trim();
+  }
+
+  public static boolean isValidOsmNameTag(String tag) {
+    return VALID_NAME_TAGS.test(tag);
+  }
+
+  public static String getLatinName(Map<String, Object> tags, boolean transliterate) {
+    String name = string(tags.get("name"));
+    if (containsOnlyLatinCharacters(name)) {
+      return name;
+    } else {
+      return getNameTranslations(tags)
+        .filter(LanguageUtils::containsOnlyLatinCharacters)
+        .findFirst()
+        .orElse(transliterate ? Translations.transliterate(name) : null);
+    }
+  }
+
+
+  private static Stream<String> getNameTranslations(Map<String, Object> tags) {
+    return Stream.concat(
+      Stream.of("name:en", "int_name", "name:de").map(tag -> string(tags.get(tag))),
+      tags.entrySet().stream()
+        .filter(e -> !EN_DE_NAME_KEYS.contains(e.getKey()) && VALID_NAME_TAGS.test(e.getKey()))
+        .map(Map.Entry::getValue)
+        .map(LanguageUtils::string)
+    );
   }
 }
