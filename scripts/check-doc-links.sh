619ac83ed81aa623ec71d61d51566b86330da52e
Michael Barry <msbarry@users.noreply.github.com>
2022-08-02 07:20:49 -0400

    Submodule followups (#312)

--- scripts/check-doc-links.sh
+++ scripts/check-doc-links.sh
@@ -4,4 +4,4 @@ set -o errexit
 set -o pipefail
 set -o nounset
 
-find . -name '*.md' -not -path '*/target/*' -print0 | xargs -I {} -n 1 -0 markdown-link-check --quiet --config .github/workflows/docs_mlc_config.json {}
+find . -name '*.md' -not -path '*/target/*' -not -path '*/planetiler-openmaptiles/*' -not -path '*/data/*' -print0 | xargs -I {} -n 1 -0 markdown-link-check --quiet --config .github/workflows/docs_mlc_config.json {}
