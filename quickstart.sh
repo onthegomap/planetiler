3ae8850b0480ec175732657172f4f84e642f7ab1
Mike Barry <msb5014@gmail.com>
2022-09-14 05:54:43 -0400

    fix quickstart

--- quickstart.sh
+++ quickstart.sh
@@ -15,6 +15,7 @@ MEMORY=""
 DRY_RUN=""
 VERSION="latest"
 DOCKER_DIR="$(pwd)/data"
+TASK="openmaptiles"
 
 # Parse args into env vars
 while [[ $# -gt 0 ]]; do
@@ -37,19 +38,35 @@ while [[ $# -gt 0 ]]; do
 
     --dry-run) DRY_RUN="true" ;;
 
-    *) PLANETILER_ARGS+=("$1") ;;
+    *)
+      # on the first passthrough arg, check if it's instructions to do something besides openmaptiles
+      if (( ${#PLANETILER_ARGS[@]} == 0 )); then
+        case $1 in
+          *.yml|*openmaptiles*|*-qa|example-*|verify-*|*custom|benchmark*)
+            TASK="$1"
+            PLANETILER_ARGS+=("$1")
+            ;;
+          -*) PLANETILER_ARGS+=("$1") ;;
+          *) AREA="$1" ;;
+        esac
+      else
+        PLANETILER_ARGS+=("$1")
+      fi
+      ;;
   esac
   shift
 done
 
-PLANETILER_ARGS+=("--area=$AREA")
 PLANETILER_ARGS+=("--storage=$STORAGE")
 PLANETILER_ARGS+=("--download")
 PLANETILER_ARGS+=("--force")
 
 # Configure memory settings based on the area being built
+PLANETILER_ARGS+=("--area=$AREA")
 case $AREA in
   planet)
+    # For extracts, use default nodemap type (sortedtable) and -Xmx (25% of RAM up to 25GB) and hope for the best.
+    # You can set --memory=5g if you want to change it.
     PLANETILER_ARGS+=("--nodemap-type=array" "--download-threads=20" "--download-chunk-size-mb=500")
     case "$STORAGE" in
       ram) MEMORY="${MEMORY:-"-Xmx150g"}" ;;
@@ -57,27 +74,27 @@ case $AREA in
     esac
     ;;
   monaco)
-    # Use mini extracts for monaco
-    PLANETILER_ARGS+=("--water-polygons-url=https://github.com/onthegomap/planetiler/raw/main/planetiler-core/src/test/resources/water-polygons-split-3857.zip")
-    PLANETILER_ARGS+=("--water-polygons-path=data/sources/monaco-water.zip")
-    PLANETILER_ARGS+=("--natural-earth-url=https://github.com/onthegomap/planetiler/raw/main/planetiler-core/src/test/resources/natural_earth_vector.sqlite.zip")
-    PLANETILER_ARGS+=("--natural-earth-path=data/sources/monaco-natural_earth_vector.sqlite.zip")
+    if [ "$TASK" == "openmaptiles" ]; then
+      # Use mini extracts for monaco
+      PLANETILER_ARGS+=("--water-polygons-url=https://github.com/onthegomap/planetiler/raw/main/planetiler-core/src/test/resources/water-polygons-split-3857.zip")
+      PLANETILER_ARGS+=("--water-polygons-path=data/sources/monaco-water.zip")
+      PLANETILER_ARGS+=("--natural-earth-url=https://github.com/onthegomap/planetiler/raw/main/planetiler-core/src/test/resources/natural_earth_vector.sqlite.zip")
+      PLANETILER_ARGS+=("--natural-earth-path=data/sources/monaco-natural_earth_vector.sqlite.zip")
+    fi
     ;;
 esac
-# For extracts, use default nodemap type (sortedtable) and -Xmx (25% of RAM up to 25GB) and hope for the best.
-# You can set --memory=5g if you want to change it.
 
 JVM_ARGS="-XX:+UseParallelGC $MEMORY"
 
 echo "Running planetiler with:"
 echo "  METHOD=\"$METHOD\" (change with --docker --jar or --build)"
 echo "  JVM_ARGS=\"${JVM_ARGS}\" (change with --memory=Xg)"
+echo "  TASK=\"${TASK}\""
 echo "  PLANETILER_ARGS=\"${PLANETILER_ARGS[*]}\""
 echo "  DRY_RUN=\"${DRY_RUN:-false}\""
 echo ""
 
-if [ "$DRY_RUN" == "true" ]
-then
+if [ "$DRY_RUN" == "true" ]; then
   echo "Without --dry-run, will run commands:"
 else
   sleep 3
@@ -85,15 +102,13 @@ fi
 
 function run() {
   echo "$ $*"
-  if [ "$DRY_RUN" != "true" ]
-  then
+  if [ "$DRY_RUN" != "true" ]; then
     eval "$*"
   fi
 }
 
 function check_java_version() {
-  if [ "$DRY_RUN" != "true" ]
-  then
+  if [ "$DRY_RUN" != "true" ]; then
     if [ -z "$(which java)" ]; then
       echo "java not found on path"
       exit 1
