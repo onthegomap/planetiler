dad2073f6174ef5b2385d515116918c4cde5005f
Michael Barry <msbarry@users.noreply.github.com>
2023-01-02 11:26:00 -0500

    Add planetiler-specific metadata (#428)

--- planetiler-core/src/main/java/com/onthegomap/planetiler/config/MbtilesMetadata.java
+++ planetiler-core/src/main/java/com/onthegomap/planetiler/config/MbtilesMetadata.java
@@ -2,6 +2,9 @@ package com.onthegomap.planetiler.config;
 
 import com.onthegomap.planetiler.Profile;
 import com.onthegomap.planetiler.mbtiles.MbtilesWriter;
+import com.onthegomap.planetiler.util.BuildInfo;
+import java.util.LinkedHashMap;
+import java.util.Map;
 
 /** Controls information that {@link MbtilesWriter} will write to the mbtiles metadata table. */
 public record MbtilesMetadata(
@@ -9,7 +12,8 @@ public record MbtilesMetadata(
   String description,
   String attribution,
   String version,
-  String type
+  String type,
+  Map<String, String> planetilerSpecific
 ) {
 
   public MbtilesMetadata(Profile profile) {
@@ -18,7 +22,8 @@ public record MbtilesMetadata(
       profile.description(),
       profile.attribution(),
       profile.version(),
-      profile.isOverlay() ? "overlay" : "baselayer"
+      profile.isOverlay() ? "overlay" : "baselayer",
+      mapWithBuildInfo()
     );
   }
 
@@ -29,7 +34,32 @@ public record MbtilesMetadata(
       args.getString("mbtiles_attribution", "'attribution' attribute for mbtiles metadata", profile.attribution()),
       args.getString("mbtiles_version", "'version' attribute for mbtiles metadata", profile.version()),
       args.getString("mbtiles_type", "'type' attribute for mbtiles metadata",
-        profile.isOverlay() ? "overlay" : "baselayer")
+        profile.isOverlay() ? "overlay" : "baselayer"),
+      mapWithBuildInfo()
     );
+  }
+
+  private static Map<String, String> mapWithBuildInfo() {
+    Map<String, String> result = new LinkedHashMap<>();
+    var buildInfo = BuildInfo.get();
+    if (buildInfo != null) {
+      if (buildInfo.version() != null) {
+        result.put("planetiler:version", buildInfo.version());
+      }
+      if (buildInfo.githash() != null) {
+        result.put("planetiler:githash", buildInfo.githash());
+      }
+      if (buildInfo.buildTimeString() != null) {
+        result.put("planetiler:buildtime", buildInfo.buildTimeString());
+      }
+    }
+    return result;
+  }
+
+  public MbtilesMetadata set(String key, Object value) {
+    if (key != null && value != null) {
+      planetilerSpecific.put(key, value.toString());
+    }
+    return this;
   }
 }
