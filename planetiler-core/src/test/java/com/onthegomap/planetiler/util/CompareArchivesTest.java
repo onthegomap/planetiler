17d93d63dc2a5690e6cccbee5bf179bd42bf1650
Mike Barry <msb5014@gmail.com>
2024-01-12 06:54:25 -0500

    improve test coverage

--- planetiler-core/src/test/java/com/onthegomap/planetiler/util/CompareArchivesTest.java
+++ planetiler-core/src/test/java/com/onthegomap/planetiler/util/CompareArchivesTest.java
@@ -6,6 +6,7 @@ import com.onthegomap.planetiler.Profile;
 import com.onthegomap.planetiler.archive.TileArchiveConfig;
 import com.onthegomap.planetiler.archive.TileArchiveMetadata;
 import com.onthegomap.planetiler.archive.TileEncodingResult;
+import com.onthegomap.planetiler.config.Arguments;
 import com.onthegomap.planetiler.config.PlanetilerConfig;
 import com.onthegomap.planetiler.geo.TileOrder;
 import com.onthegomap.planetiler.pmtiles.WriteablePmtiles;
@@ -81,6 +82,60 @@ class CompareArchivesTest {
         "archive 2 missing tile", 1L,
         "archive 1 missing tile", 2L,
         "different contents", 1L
+      ), Map.of(
+        "layer1", Map.of(
+          "values list unique values", 1L,
+          "feature ids", 1L
+        )
+      )
+    ), result);
+  }
+
+  @Test
+  void testCompareArchivesDifferentCompression() throws IOException {
+    var aPath = path.resolve("a.pmtiles");
+    var bPath = path.resolve("b.pmtiles");
+    byte[] a1 = new byte[]{0xa, 0x2};
+    byte[] b1 = Gzip.gzip(a1);
+    byte[] a2 = tile1;
+    byte[] b2 = Gzip.gzip(tile2);
+    try (
+      var a = WriteablePmtiles.newWriteToFile(aPath);
+      var b = WriteablePmtiles.newWriteToFile(bPath);
+    ) {
+      a.initialize();
+      b.initialize();
+      try (
+        var aWriter = a.newTileWriter();
+        var bWriter = b.newTileWriter()
+      ) {
+        aWriter
+          .write(new TileEncodingResult(TileOrder.HILBERT.decode(0), a1, OptionalLong.empty()));
+        aWriter
+          .write(new TileEncodingResult(TileOrder.HILBERT.decode(2), a2, OptionalLong.empty()));
+        aWriter
+          .write(new TileEncodingResult(TileOrder.HILBERT.decode(4), a1, OptionalLong.empty()));
+        bWriter.write(new TileEncodingResult(TileOrder.HILBERT.decode(1), b1, OptionalLong.empty()));
+        bWriter.write(new TileEncodingResult(TileOrder.HILBERT.decode(2), b2, OptionalLong.empty()));
+        bWriter.write(new TileEncodingResult(TileOrder.HILBERT.decode(3), b1, OptionalLong.empty()));
+        bWriter
+          .write(new TileEncodingResult(TileOrder.HILBERT.decode(4), b1, OptionalLong.empty()));
+      }
+      a.finish(new TileArchiveMetadata(new Profile.NullProfile(),
+        PlanetilerConfig.from(Arguments.fromArgs("--tile-compression=none"))));
+      b.finish(new TileArchiveMetadata(new Profile.NullProfile(), config));
+    }
+    var result = CompareArchives.compare(
+      TileArchiveConfig.from(aPath.toString()),
+      TileArchiveConfig.from(bPath.toString()),
+      config,
+      false
+    );
+    assertEquals(new CompareArchives.Result(
+      5, 4, Map.of(
+        "archive 2 missing tile", 1L,
+        "archive 1 missing tile", 2L,
+        "different decompressed contents", 1L
       ), Map.of(
         "layer1", Map.of(
           "values list unique values", 1L,
