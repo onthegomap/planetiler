8c4e1fc7d3c1732326a3353780c192da1047a485
Dave Craig <davecraig@unbalancedaudio.com>
2024-11-08 12:14:11 +0000

    Shortcut through simplification step if distanceTolerance is negative
    If the simplification tolerance has been set to less than 0.0 then there's
    no need to run the DouglasPeuckerSimplifier algorithm. This change
    checks the tolerance and simply returns a copy of the Geometry if no
    simplification is requested.
    This change is required for the case where the user wants no
    simplification to take place e.g. to preserve all intersection nodes.
    Without this change any negative value for distanceTolerance is squared
    inside DPTransformer and immediately loses its meaning.

--- planetiler-core/src/test/java/com/onthegomap/planetiler/FeatureMergeTest.java
+++ planetiler-core/src/test/java/com/onthegomap/planetiler/FeatureMergeTest.java
@@ -165,6 +165,21 @@ class FeatureMergeTest {
         true
       )
     );
+    // but doesn't resimplify if the tolerance is negative even when resimplify=true
+    assertEquals(
+      List.of(
+        feature(1, newLineString(10, 10, 20, 20, 30, 30), Map.of("a", 1))
+      ),
+      FeatureMerge.mergeLineStrings(
+        List.of(
+          feature(1, newLineString(10, 10, 20, 20, 30, 30), Map.of("a", 1))
+        ),
+        0,
+        -1,
+        0,
+        true
+      )
+    );
   }
 
   @Test
