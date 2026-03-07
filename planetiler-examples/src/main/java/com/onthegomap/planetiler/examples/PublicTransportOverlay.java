e065225a387deb89058a8b764d487b7d88d38fc3
BY3D <49314704+BY3D@users.noreply.github.com>
2026-03-06 21:12:37 -0500

    Create PublicTransportOverlay.java

---
+++ planetiler-examples/src/main/java/com/onthegomap/planetiler/examples/PublicTransportOverlay.java
@@ -0,0 +1,30 @@
+package com.onthegomap.planetiler.examples;
+
+import com.onthegomap.planetiler.FeatureCollector;
+import com.onthegomap.planetiler.Profile;
+import com.onthegomap.planetiler.reader.SourceFeature;
+
+public class PublicTransportOverlay implements Profile {
+
+  @Override
+  public void processFeature(SourceFeature sourceFeature, FeatureCollector features) {
+    //
+  }
+
+  @Override
+  public String name() {
+    // name that shows up in the MBTiles metadata table
+    return "Public Transport Overlay";
+  }
+
+  public static void main(String... args) throws Exception {
+    /*
+    Planetiler.create(args)
+      .setProfile(new MyProfile())
+      // if input.pbf not found, download Monaco from Geofabrik
+      .addOsmSource("osm", Path.of("data", "sources", "input.pbf"), "geofabrik:monaco")
+      .overwriteOutput("mbtiles", Path.of("data", "toilets.mbtiles"))
+      .run();
+     */
+  }
+}
