18357051a81729c94e375c4772affd9193040539
BY3D <49314704+BY3D@users.noreply.github.com>
2026-03-07 14:29:52 -0500

    Add description, isOverlay, and attribution methods

--- planetiler-examples/src/main/java/com/onthegomap/planetiler/examples/PublicTransportOverlay.java
+++ planetiler-examples/src/main/java/com/onthegomap/planetiler/examples/PublicTransportOverlay.java
@@ -8,13 +8,36 @@ public class PublicTransportOverlay implements Profile {
 
   @Override
   public void processFeature(SourceFeature sourceFeature, FeatureCollector features) {
-    //
+    if (sourceFeature.canBeLine()) {
+      //
+    }
+  }
+
+  @Override
+  public boolean isOverlay() {
+    return true;
   }
 
   @Override
   public String name() {
     // name that shows up in the MBTiles metadata table
     return "Public Transport Overlay";
+  }
+
+  @Override
+  public String description() {
+    return "An example overlay that shows tram routes and stops";
+  }
+
+  /*
+   * Any time you use OpenStreetMap data, you must ensure clients display the following copyright. Most clients will
+   * display this automatically if you populate it in the attribution metadata in the mbtiles file:
+   */
+  @Override
+  public String attribution() {
+    return """
+      <a href="https://www.openstreetmap.org/copyright" target="_blank">&copy; OpenStreetMap contributors</a>
+      """.trim();
   }
 
   public static void main(String[] args) throws Exception {
