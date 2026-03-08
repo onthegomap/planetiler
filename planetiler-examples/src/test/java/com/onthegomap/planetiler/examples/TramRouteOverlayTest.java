4d2b8945a36facabc5419b02b5e4085230ee7c09
BY3D <49314704+BY3D@users.noreply.github.com>
2026-03-08 12:08:05 -0400

    Add simple unit test for tram_stop node
    The unit test only checks for layer type and zoom level for now

--- planetiler-examples/src/test/java/com/onthegomap/planetiler/examples/TramRouteOverlayTest.java
+++ planetiler-examples/src/test/java/com/onthegomap/planetiler/examples/TramRouteOverlayTest.java
@@ -1,8 +1,35 @@
 package com.onthegomap.planetiler.examples;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import com.onthegomap.planetiler.FeatureCollector;
+import com.onthegomap.planetiler.TestUtils;
+import com.onthegomap.planetiler.reader.SimpleFeature;
+import java.util.List;
+import java.util.Map;
 import org.junit.jupiter.api.Test;
 
 public class TramRouteOverlayTest {
+
+  private final TramRouteOverlay profile = new TramRouteOverlay();
+
+  @Test
+  public void testSourceFeatureProcessing() {
+    // Test tram stops first, then tram routes
+    var node = SimpleFeature.create(
+      TestUtils.newPoint(5, 4),
+      Map.of("railway", "tram_stop")
+    );
+    // Produce a one-element list with a node containing the tag railway=tram_stop
+    List<FeatureCollector.Feature> mapFeatures = TestUtils.processSourceFeature(node, profile);
+    // Assertion cases
+    assertEquals(1, mapFeatures.size());
+    var feature = mapFeatures.get(0);
+    assertEquals("tram_stop", feature.getLayer());
+    assertEquals(11, feature.getMinZoom());
+    // Create a fake route as well and test that in this unit test
+  }
+
 
   @Test
   public void unitTest() {
