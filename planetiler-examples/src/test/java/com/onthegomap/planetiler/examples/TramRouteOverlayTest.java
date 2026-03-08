a85226b474c48e504e05ca554eb8a0110abebaba
BY3D <49314704+BY3D@users.noreply.github.com>
2026-03-08 12:39:57 -0400

    Change access modifier of unit test

--- planetiler-examples/src/test/java/com/onthegomap/planetiler/examples/TramRouteOverlayTest.java
+++ planetiler-examples/src/test/java/com/onthegomap/planetiler/examples/TramRouteOverlayTest.java
@@ -14,7 +14,7 @@ public class TramRouteOverlayTest {
   private final TramRouteOverlay profile = new TramRouteOverlay();
 
   @Test
-  public void testSourceFeatureProcessing() {
+  void testSourceFeatureProcessing() {
     // Test tram stops first, then tram routes
     var node = SimpleFeature.create(
       TestUtils.newPoint(5, 4),
@@ -22,9 +22,8 @@ public class TramRouteOverlayTest {
     );
     // Produce a one-element list with a node containing the tag railway=tram_stop
     List<FeatureCollector.Feature> mapFeatures = TestUtils.processSourceFeature(node, profile);
-    // Assertion cases
     assertEquals(1, mapFeatures.size());
-    var feature = mapFeatures.get(0);
+    var feature = mapFeatures.getFirst();
     assertEquals("tram_stop", feature.getLayer());
     assertEquals(11, feature.getMinZoom());
     // Create a fake route as well and test that in this unit test
